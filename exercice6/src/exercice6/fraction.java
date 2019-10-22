38d60e197470a6391f178182056b45d569efb803
essaadi ahmed <33197075+ahmedglas@users.noreply.github.com>
2019-10-22 16:00:52 +0200

    Update fraction.java

--- exercice6/src/exercice6/fraction.java
+++ exercice6/src/exercice6/fraction.java
@@ -4,54 +4,6 @@ public class fraction{
 	private int denominateur;
 	private int numerateur ;
 
-	fraction (int num , int d ){
-		denominateur=num;
-		numerateur =d;
-	}
-
-	fraction (int num)
-	{
-		denominateur=1;
-		numerateur=num;
-	}
-	fraction(){
-		numerateur=0;
-				denominateur=1;
-	}
-		
-	public final fraction zero = new fraction(0,1);
-	public final fraction un = new fraction (1,1);
 	
-	public int getDenumerateur(){
-		return	this.denominateur;
-	}
-	
-	public int getNumerateur() {
-		return this.numerateur;
-	}
-	
-	public double getRes(){
-		return ((double)numerateur/(double)denominateur);
-	}
-	
-	public String toString(){
-		 String res = String.valueOf(numerateur) + "/" + String.valueOf(denominateur);
-		 return res;
-	}
-
-	public boolean testFraction(fraction a)
-	{
-	if( this.getRes()==a.getRes())
-	{
-		return true;
-	}
-	else return false;
-	}
-	
-	public fraction compare(fraction f) {
-		if(this.getRes() > f.getRes())
-			return this;
-		return f;
-	}
 }
 

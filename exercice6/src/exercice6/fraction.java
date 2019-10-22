5abf12a1e239139757a63d670696d3bbc6cceae2
Helmi Abed <abed.helmi@yahoo.fr>
2019-10-22 16:10:02 +0200

    constructeur ya bhim

--- exercice6/src/exercice6/fraction.java
+++ exercice6/src/exercice6/fraction.java
@@ -3,6 +3,19 @@ package exercice6;
 public class fraction{
 	private int denominateur;
 	private int numerateur ;
+	
+	
+	public fraction (){
+		
+		this.denominateur=0;
+		this.numerateur=0;
+	}
+	
+		public fraction (int x,int y){
+		
+		this.denominateur=x;
+		this.numerateur=y;
+	}
 
 	
 }
